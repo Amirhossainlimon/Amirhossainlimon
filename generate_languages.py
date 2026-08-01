9916a9ca10ef1d0a06338c88fde6f04bc14cb603
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2026-08-01 22:31:38 +0600

    Update generate_languages.py

--- generate_languages.py
+++ generate_languages.py
@@ -1,13 +1,16 @@
 import os
 import requests
 
-username = "AmirhossainLimon"
+
+username = "Amirhossainlimon"
 
 token = os.getenv("GITHUB_TOKEN")
+
 
 headers = {
     "Authorization": f"Bearer {token}"
 }
+
 
 repos = requests.get(
     f"https://api.github.com/users/{username}/repos?per_page=100",
@@ -15,59 +18,169 @@ repos = requests.get(
 ).json()
 
 
+
 languages = {}
 
+
 for repo in repos:
-    url = repo["languages_url"]
 
     data = requests.get(
-        url,
+        repo["languages_url"],
         headers=headers
     ).json()
 
+
     for lang, value in data.items():
-        languages[lang] = languages.get(lang, 0) + value
+
+        languages[lang] = languages.get(lang,0)+value
+
 
 
 total = sum(languages.values())
 
-svg = """
-<svg width="500" height="300" xmlns="http://www.w3.org/2000/svg">
 
-<rect width="100%" height="100%" fill="#0d1117"/>
-
-<text x="30" y="40" fill="white" font-size="24">
-🔥 Most Used Languages
-</text>
-"""
-
-
-y = 80
-
-for lang, value in sorted(
+top_languages = sorted(
     languages.items(),
     key=lambda x:x[1],
     reverse=True
-)[:6]:
+)[:6]
 
-    percent = round(value / total * 100, 1)
+
+
+svg = """
+
+<svg width="600" height="360"
+xmlns="http://www.w3.org/2000/svg">
+
+
+<style>
+
+.title{
+font-family:Arial;
+font-size:24px;
+font-weight:bold;
+fill:white;
+}
+
+
+.text{
+font-family:Arial;
+font-size:18px;
+fill:white;
+}
+
+
+.bar{
+
+animation:grow 2s ease;
+
+}
+
+
+@keyframes grow{
+
+from{
+width:0;
+}
+
+to{
+width:100%;
+}
+
+}
+
+
+</style>
+
+
+
+<rect width="100%" height="100%" rx="20" fill="#0d1117"/>
+
+
+
+<text x="30" y="45" class="title">
+
+🔥 Most Used Languages
+
+</text>
+
+"""
+
+
+y = 90
+
+
+
+for lang,value in top_languages:
+
+
+    percent = round(
+        (value/total)*100,
+        1
+    )
+
+
+    width = percent * 4
+
+
 
     svg += f"""
-    <text x="30" y="{y}" fill="white" font-size="18">
-    {lang}: {percent}%
-    </text>
-    """
-
-    y += 35
 
 
-svg += "</svg>"
+<text x="30" y="{y}" class="text">
+
+{lang} - {percent}%
+
+</text>
 
 
-os.makedirs("assets", exist_ok=True)
+<rect
+
+x="220"
+
+y="{y-18}"
+
+height="16"
+
+width="{width}"
+
+rx="8"
+
+fill="#02569B"
+
+class="bar"
+
+/>
+
+
+"""
+
+    y += 45
+
+
+
+svg += """
+
+</svg>
+
+"""
+
+
+
+os.makedirs(
+    "assets",
+    exist_ok=True
+)
+
+
 
 with open(
     "assets/languages.svg",
     "w"
-) as f:
-    f.write(svg)
+) as file:
+
+    file.write(svg)
+
+
+
+print("Language SVG Generated Successfully")
